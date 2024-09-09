04d63117e1852e784a563802dd2edb5260db3b1c
luisfeliperbarbosa <luisfelipebr844@gmail.com>
2024-09-08 22:58:47 -0300

    adicionar endpoint integrantes

---
+++ src/index.js
@@ -0,0 +1,26 @@
+const express = require('express') 
+const cors = require('cors')
+const app = express()
+
+const port = 3333
+
+const data = {
+    'integrantes':[
+        {'nome':'Luis Felipe Rodrigues Barbosa'},
+        {'nome':'Lucas Zikan'},
+        {'nome':'Luiz Otavio Amboni'},
+    ],
+}
+
+app.use(express.json())
+app.use(cors())
+
+app.get('/', (req, res) => {
+    res.sendFile('./views/index.html', { root: __dirname })
+})
+
+app.get('/integrantes', (req, res) => {
+    res.json(data)
+})
+
+app.listen(port, () => console.log('O servidor está rodando na porta 3333'))
